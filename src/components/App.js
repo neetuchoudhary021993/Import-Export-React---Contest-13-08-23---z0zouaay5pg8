414f14917d0c5075c243522fa5f06bd0090d40ad
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-13 10:05:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
   )
 }
-
+export default App;
 
